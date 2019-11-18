549ec0fbf9a2c0ef928a3e69d30c575d1444c990
Kory Newton <korybnewton@gmail.com>
2019-11-18 10:31:16 -0800

    add react-router-dom and implement basic routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './pages/home/home.component';
+import Order from './pages/order/order.component';
+import Orders from './pages/orders/orders.component';
 
 function App() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/order" component={Order} />
+        <Route path="/orders" component={Orders} />
+      </Switch>
     </div>
   );
 }
