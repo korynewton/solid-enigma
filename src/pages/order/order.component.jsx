17b78aa68fb9d7af68f9e2002031aa3e1dce223b
Kory Newton <korybnewton@gmail.com>
2019-11-18 12:04:57 -0800

    retrieve options from backend api

--- src/pages/order/order.component.jsx
+++ src/pages/order/order.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 import CupcakeCustomizer from '../../components/cupcake-cusomizer/cupcake-customizer.component';
 import AddCupcake from '../../components/add-cupcake/add-cupcake.component';
@@ -7,18 +8,38 @@ import ReviewOrder from '../../components/review-order/review-order.component';
 class Order extends React.Component {
   state = {
     bases: [],
-    frosting: [],
+    frostings: [],
     toppings: [],
-    order: {},
+    order: [],
     viewOrder: false
   };
 
+  async componentDidMount() {
+    // Retrieve cupcake base, frosting and topping options, set state
+    const baseURL = 'http://localhost:4000/cupcakes';
+    const [basesRes, frostingsRes, toppingsRes] = await Promise.all([
+      axios.get(`${baseURL}/bases`),
+      axios.get(`${baseURL}/frostings`),
+      axios.get(`${baseURL}/toppings`)
+    ]);
+
+    this.setState({
+      bases: basesRes.data.bases,
+      frostings: frostingsRes.data.frostings,
+      toppings: toppingsRes.data.toppings
+    });
+  }
+
   render() {
-    const { viewOrder } = this.state;
+    const { bases, frostings, toppings, viewOrder } = this.state;
 
     return !viewOrder ? (
       <div>
-        <CupcakeCustomizer />
+        <CupcakeCustomizer
+          bases={bases}
+          frostings={frostings}
+          toppings={toppings}
+        />
         <AddCupcake />
       </div>
     ) : (
