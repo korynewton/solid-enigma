ab25040491d1b379872f62072fe6e4b4b8a77773
Kory Newton <korybnewton@gmail.com>
2019-11-21 10:19:17 -0800

    home page styling

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import Home from './pages/home/home.component';
 import Order from './pages/order/order.component';
 import Orders from './pages/orders/orders.component';
 
+import './App.styles.scss';
+
 function App() {
   return (
     <div>
+      <h1 className="header">Tinycakes</h1>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/order" component={Order} />
--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import cupcake from '../../images/cupcake.png';
+
+import './home.styles.scss';
 
 const Home = () => {
   return (
     <div>
-      <h2>Home Page</h2>
+      <div className="image">
+        <img src={cupcake} alt="cupcake" />
+      </div>
+      <div className="options">
+        <Link to="/order">
+          <button>Start a New Order</button>
+        </Link>
+        <Link to="/orders">
+          <button>View Past Orders</button>
+        </Link>
+      </div>
     </div>
   );
 };
